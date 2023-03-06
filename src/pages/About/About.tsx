2ed8a869c0b23813c0e8582569d71d40be43ab36
Dima <dima.tsymbal@outlook.com>
2023-03-06 19:00:10 +0200

    text block in about

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,10 +1,12 @@
 import Slider_block from './Slider_about/Slider_block'
 import Map from './Map/Map'
+import Text_block from './Text_block/Text_block'
 type Props = {}
 const About = (props: Props) => {
     return (
         <>
             <Slider_block />
+            <Text_block />
             <Map />
         </>
     )
