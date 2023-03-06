2d3e3d43bba912f2b1d70b7af606fc5eaae6d62f
Dima <dima.tsymbal@outlook.com>
2023-03-06 18:26:13 +0200

    header dropdown and all adaptive

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -1,13 +1,13 @@
 import './header-style.css'
 
 import AppBar from '@mui/material/AppBar'
-import Box from '@mui/material/Box'
 import Toolbar from '@mui/material/Toolbar'
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth'
 import CallIcon from '@mui/icons-material/Call'
 import Menu from 'components/Menu/Menu'
 import Logo from 'components/Logo/Logo'
 import { Button } from '@mui/material'
+import MenuItem from 'components/Menu/MenuItem'
 
 type Props = {}
 const Header = (props: Props) => {
@@ -21,14 +21,18 @@ const Header = (props: Props) => {
                     <CallIcon /> Call Us - (+22) 123 456 7890
                 </Button>
 
-                <Menu />
-
-                <div className="links_in_adaptive">
-                    <button className="burger_btn_header">
+                <div className="dropdown">
+                    <a
+                        className="btn dropdown-toggle"
+                        href="#"
+                        role="button"
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                    >
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
-                            width="26"
-                            height="26"
+                            width="16"
+                            height="16"
                             fill="currentColor"
                             className="bi bi-list"
                             viewBox="0 0 16 16"
@@ -38,8 +42,29 @@ const Header = (props: Props) => {
                                 d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
                             />
                         </svg>
-                    </button>
+                    </a>
+
+                    <ul className="dropdown-menu">
+                        <li>
+                            <MenuItem to="/">Home</MenuItem>
+                        </li>
+                        <li>
+                            <MenuItem to="about">About</MenuItem>
+                        </li>
+                        <li>
+                            <MenuItem to="/services">Our Services</MenuItem>
+                        </li>
+                        <li>
+                            <MenuItem to="/contact">Contact Us</MenuItem>
+                        </li>
+                        <Button className="call_btn_header_adaptive">
+                            {' '}
+                            <CallIcon /> Call Us - (+22) 123 456 7890
+                        </Button>
+                    </ul>
                 </div>
+
+                <Menu />
 
                 <button className="reservation_btn_header">
                     <CalendarMonthIcon />
